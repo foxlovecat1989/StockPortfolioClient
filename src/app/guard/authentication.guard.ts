3e022e361f401f9cf78a466a32fe99acc4c9829e
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-12-18 04:27:19 +0800

    implements notification feature

--- src/app/guard/authentication.guard.ts
+++ src/app/guard/authentication.guard.ts
@@ -1,24 +1,28 @@
-
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
+import { NotificationService } from '../service/notification.service';
+import { NotificationType } from '../enum/notification-type.enum';
 
 @Injectable({providedIn: 'root'})
 export class AuthenticationGuard implements CanActivate {
 
   constructor(
-    private authenticationService: AuthenticationService
-    ) {}
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     return this.isUserLoggedIn();
   }
 
   private isUserLoggedIn(): boolean {
-    if (this.authenticationService.isUserLoggedIn()) {
+    if (this.authenticationService.isUserLoggedIn())
       return true;
-    }
-    // TODO: noticify the user they should login
+    this.router.navigate(['/login']);
+    this.notificationService.notify(NotificationType.ERROR, `You need to log in to access this page`);
+
     return false;
   }
 
