8b8b92a44b75a34e18108df0be11c8515a9d8269
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-12-18 04:06:49 +0800

    implements AuthenticationGuard

---
+++ src/app/guard/authentication.guard.ts
@@ -0,0 +1,25 @@
+
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthenticationService } from '../service/authentication.service';
+
+@Injectable({providedIn: 'root'})
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(
+    private authenticationService: AuthenticationService
+    ) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.isUserLoggedIn();
+  }
+
+  private isUserLoggedIn(): boolean {
+    if (this.authenticationService.isUserLoggedIn()) {
+      return true;
+    }
+    // TODO: noticify the user they should login
+    return false;
+  }
+
+}
